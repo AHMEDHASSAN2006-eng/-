cd9e5843c044b11b129f1ce7d745dc4c916632fc
AHMEDHASSAN2006-eng <eitaaabahmed@gmail.com>
2025-11-05 00:37:23 +0200

    Add files via upload

---
+++ مشروع د _ شريف/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace مشروع_دكتور_شريف
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numEquations;
+        private System.Windows.Forms.Button btnGenerateMatrix;
+        private System.Windows.Forms.Panel matrixPanel;
+        private System.Windows.Forms.Button btnSolve;
+        private System.Windows.Forms.RichTextBox stepsTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numEquations = new System.Windows.Forms.NumericUpDown();
+            this.btnGenerateMatrix = new System.Windows.Forms.Button();
+            this.matrixPanel = new System.Windows.Forms.Panel();
+            this.btnSolve = new System.Windows.Forms.Button();
+            this.stepsTextBox = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numEquations)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(408, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "عدد المعادلات:";
+            // 
+            // numEquations
+            // 
+            this.numEquations.Location = new System.Drawing.Point(292, 25);
+            this.numEquations.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numEquations.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numEquations.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numEquations.Name = "numEquations";
+            this.numEquations.Size = new System.Drawing.Size(93, 24);
+            this.numEquations.TabIndex = 1;
+            this.numEquations.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // btnGenerateMatrix
+            // 
+            this.btnGenerateMatrix.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnGenerateMatrix.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGenerateMatrix.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.btnGenerateMatrix.ForeColor = System.Drawing.Color.White;
+            this.btnGenerateMatrix.Location = new System.Drawing.Point(134, 25);
+            this.btnGenerateMatrix.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnGenerateMatrix.Name = "btnGenerateMatrix";
+            this.btnGenerateMatrix.Size = new System.Drawing.Size(140, 37);
+            this.btnGenerateMatrix.TabIndex = 2;
+            this.btnGenerateMatrix.Text = "إنشاء مصفوفة المعادلات";
+            this.btnGenerateMatrix.UseVisualStyleBackColor = false;
+            this.btnGenerateMatrix.Click += new System.EventHandler(this.btnGenerateMatrix_Click);
+            // 
+            // matrixPanel
+            // 
+            this.matrixPanel.AutoScroll = true;
+            this.matrixPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.matrixPanel.Location = new System.Drawing.Point(23, 74);
+            this.matrixPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.matrixPanel.Name = "matrixPanel";
+            this.matrixPanel.Size = new System.Drawing.Size(525, 246);
+            this.matrixPanel.TabIndex = 3;
+            this.matrixPanel.Visible = false;
+            // 
+            // btnSolve
+            // 
+            this.btnSolve.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnSolve.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSolve.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.btnSolve.ForeColor = System.Drawing.Color.White;
+            this.btnSolve.Location = new System.Drawing.Point(23, 332);
+            this.btnSolve.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSolve.Name = "btnSolve";
+            this.btnSolve.Size = new System.Drawing.Size(525, 43);
+            this.btnSolve.TabIndex = 4;
+            this.btnSolve.Text = "حل المعادلات";
+            this.btnSolve.UseVisualStyleBackColor = false;
+            this.btnSolve.Visible = false;
+            this.btnSolve.Click += new System.EventHandler(this.btnSolve_Click);
+            // 
+            // stepsTextBox
+            // 
+            this.stepsTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.stepsTextBox.Location = new System.Drawing.Point(23, 418);
+            this.stepsTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.stepsTextBox.Name = "stepsTextBox";
+            this.stepsTextBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.stepsTextBox.Size = new System.Drawing.Size(524, 245);
+            this.stepsTextBox.TabIndex = 5;
+            this.stepsTextBox.Text = "";
+            this.stepsTextBox.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label2.Location = new System.Drawing.Point(210, 382);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "خطوات الحل:";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label3.Location = new System.Drawing.Point(175, -37);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(461, 29);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "مشروع دكتور شريف - Gaussian Elimination";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 690);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.stepsTextBox);
+            this.Controls.Add(this.btnSolve);
+            this.Controls.Add(this.matrixPanel);
+            this.Controls.Add(this.btnGenerateMatrix);
+            this.Controls.Add(this.numEquations);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.Text = "Gaussian Elimination - دكتور شريف";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numEquations)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
+
---
+++ مشروع د _ شريف/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+//using System.Reflection.Emit;
+using System.Windows.Forms;
+
+namespace مشروع_دكتور_شريف
+{
+    public partial class Form1 : Form
+    {
+        private List<List<TextBox>> matrixInputs = new List<List<TextBox>>();
+        private int equationCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "مشروع دكتور شريف - Gaussian Elimination";
+        }
+
+        private void btnGenerateMatrix_Click(object sender, EventArgs e)
+        {
+            equationCount = (int)numEquations.Value;
+            GenerateMatrixInputs(equationCount);
+            matrixPanel.Visible = true;
+            btnSolve.Visible = true;
+            stepsTextBox.Visible = false;
+            label2.Visible = false;
+        }
+
+        private void GenerateMatrixInputs(int n)
+        {
+            matrixPanel.Controls.Clear();
+            matrixInputs.Clear();
+
+            int cellWidth = 60;
+            int cellHeight = 25;
+            int startX = 20;
+            int startY = 20;
+
+            for (int col = 0; col < n; col++)
+            {
+                Label header = new Label();
+                header.Text = $"x{col + 1}";
+                header.Size = new Size(cellWidth, cellHeight);
+                header.Location = new Point(startX + col * cellWidth, startY);
+                header.TextAlign = ContentAlignment.MiddleCenter;
+                header.Font = new Font("Microsoft Sans Serif", 8, FontStyle.Bold);
+                matrixPanel.Controls.Add(header);
+            }
+
+            Label resultHeader = new Label();
+            resultHeader.Text = "النتيجة";
+            resultHeader.Size = new Size(cellWidth, cellHeight);
+            resultHeader.Location = new Point(startX + n * cellWidth, startY);
+            resultHeader.TextAlign = ContentAlignment.MiddleCenter;
+            resultHeader.Font = new Font("Microsoft Sans Serif", 8, FontStyle.Bold);
+            matrixPanel.Controls.Add(resultHeader);
+
+            for (int row = 0; row < n; row++)
+            {
+                List<TextBox> rowInputs = new List<TextBox>();
+
+                for (int col = 0; col <= n; col++)
+                {
+                    TextBox textBox = new TextBox();
+                    textBox.Size = new Size(cellWidth, cellHeight);
+                    textBox.Location = new Point(startX + col * cellWidth, startY + (row + 1) * cellHeight + 5);
+                    textBox.Text = "0";
+                    textBox.TextAlign = HorizontalAlignment.Center;
+                    textBox.Tag = new Point(row, col);
+
+                    matrixPanel.Controls.Add(textBox);
+                    rowInputs.Add(textBox);
+                }
+                matrixInputs.Add(rowInputs);
+            }
+        }
+
+        private void btnSolve_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double[,] matrix = GetMatrixFromInputs();
+                List<string> steps = new List<string>();
+
+                steps.Add("المصفوفة الأصلية:");
+                steps.Add(MatrixToString(matrix));
+
+                double[] solution = GaussianElimination(matrix, steps);
+
+                steps.Add("\nالحل النهائي:");
+                for (int i = 0; i < solution.Length; i++)
+                {
+                    steps.Add($"x{i + 1} = {solution[i]:F4}");
+                }
+
+                DisplaySteps(steps);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"حدث خطأ: {ex.Message}", "خطأ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private double[,] GetMatrixFromInputs()
+        {
+            double[,] matrix = new double[equationCount, equationCount + 1];
+
+            for (int i = 0; i < equationCount; i++)
+            {
+                for (int j = 0; j <= equationCount; j++)
+                {
+                    if (!double.TryParse(matrixInputs[i][j].Text, out matrix[i, j]))
+                    {
+                        throw new Exception($"قيمة غير صحيحة في الصف {i + 1}, العمود {j + 1}");
+                    }
+                }
+            }
+
+            return matrix;
+        }
+
+        private double[] GaussianElimination(double[,] matrix, List<string> steps)
+        {
+            int n = equationCount;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (matrix[i, i] == 0)
+                {
+                    int swapRow = -1;
+                    for (int j = i + 1; j < n; j++)
+                    {
+                        if (matrix[j, i] != 0)
+                        {
+                            swapRow = j;
+                            break;
+                        }
+                    }
+
+                    if (swapRow != -1)
+                    {
+                        steps.Add($"\nتبديل الصف {i + 1} مع الصف {swapRow + 1}:");
+                        for (int k = 0; k <= n; k++)
+                        {
+                            double temp = matrix[i, k];
+                            matrix[i, k] = matrix[swapRow, k];
+                            matrix[swapRow, k] = temp;
+                        }
+                        steps.Add(MatrixToString(matrix));
+                    }
+                    else
+                    {
+                        throw new Exception("لا يمكن إيجاد حل فريد للنظام");
+                    }
+                }
+
+                if (matrix[i, i] != 1)
+                {
+                    double pivot = matrix[i, i];
+                    steps.Add($"\nقسمة الصف {i + 1} على {pivot:F4}:");
+                    for (int j = i; j <= n; j++)
+                    {
+                        matrix[i, j] /= pivot;
+                    }
+                    steps.Add(MatrixToString(matrix));
+                }
+
+                for (int j = i + 1; j < n; j++)
+                {
+                    double factor = matrix[j, i];
+                    if (factor != 0)
+                    {
+                        steps.Add($"\nطرح {factor:F4} × (الصف {i + 1}) من الصف {j + 1}:");
+                        for (int k = i; k <= n; k++)
+                        {
+                            matrix[j, k] -= factor * matrix[i, k];
+                        }
+                        steps.Add(MatrixToString(matrix));
+                    }
+                }
+            }
+
+            double[] solution = new double[n];
+            steps.Add("\nالاستبدال الخلفي:");
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                solution[i] = matrix[i, n];
+                for (int j = i + 1; j < n; j++)
+                {
+                    solution[i] -= matrix[i, j] * solution[j];
+                }
+                solution[i] = solution[i] / matrix[i, i];
+                steps.Add($"x{i + 1} = {solution[i]:F4}");
+            }
+
+            return solution;
+        }
+
+        private string MatrixToString(double[,] matrix)
+        {
+            string result = "";
+            int n = equationCount;
+
+            for (int i = 0; i < n; i++)
+            {
+                result += "[ ";
+                for (int j = 0; j <= n; j++)
+                {
+                    result += $"{matrix[i, j],8:F4} ";
+                }
+                result += "]\n";
+            }
+
+            return result;
+        }
+
+        private void DisplaySteps(List<string> steps)
+        {
+            stepsTextBox.Text = string.Join("\n", steps);
+            stepsTextBox.Visible = true;
+            label2.Visible = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
